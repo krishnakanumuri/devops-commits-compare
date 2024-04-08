e0adbdc726adc8c2dca0234f971eae44d528ebeb
KANUMURI Krishna <krishna.kanumuri@hexagon.com>
2024-04-08 17:24:30 +1000

    first release

---
+++ src/comparecommits/comparecommits.ts
@@ -0,0 +1,55 @@
+import "es6-promise/auto";
+import * as SDK from "azure-devops-extension-sdk";
+import { GitServiceIds, IVersionControlRepositoryService } from "azure-devops-extension-api/Git";
+import { CommonServiceIds, getClient, IHostPageLayoutService } from "azure-devops-extension-api";
+import { combineUrlPaths } from "azure-devops-extension-api/Common/Util/Url"
+
+declare global {
+  interface Window {
+    sourceCommitId: string;
+  }
+}
+
+export interface GitCommit {
+  comment: string;
+  commitId: string;
+  readonly: string;
+}
+
+export interface ActionContext {
+  commit: GitCommit;
+  serviceBaseUrl: string;
+}
+
+SDK.register("source-commit-menu", () => {
+  return {
+    execute: async (context: ActionContext) => {
+      const gitService = await SDK.getService<IVersionControlRepositoryService>(GitServiceIds.VersionControlRepositoryService);
+      const result = await gitService.getCurrentGitRepository();
+      //TODO: Implement commits compare
+      window.sourceCommitId = context.commit.commitId;
+      console.log(context.commit.commitId);
+      console.log(result);
+      console.log(context);
+    }
+  }
+});
+
+SDK.register("target-commit-menu", () => {
+  return {
+    execute: async (context: ActionContext) => {
+      const dialogSvc = await SDK.getService<IHostPageLayoutService>(CommonServiceIds.HostPageLayoutService);
+      if (window.sourceCommitId == null || window.sourceCommitId == undefined) {
+        dialogSvc.openMessageDialog("Please select source commit first", { showCancel: false });
+        return;
+      }
+      const gitService = await SDK.getService<IVersionControlRepositoryService>(GitServiceIds.VersionControlRepositoryService);
+      const result = await gitService.getCurrentGitRepository();
+      const url = combineUrlPaths("https://dev.azure.com", result!.url, `branchcompare?baseVersion=GC${window.sourceCommitId}&targetVersion=GC${context.commit.commitId}&_a=files`);
+      console.log(url);
+      window.open(url);
+    }
+  }
+});
+
+SDK.init();
